5fc0256c30eee85b2930f7c69d84672c2d424728
gdlyeabkov <gdlyeabkov@gmail.com>
2021-09-25 18:33:23 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,168 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div style="background-color: rgb(0, 0, 155); width: 100%; height: 50px;">
+      <div style="display: flex; flex-direction: row; justify-content: space-between; align-items: center;">
+        <div style="align-self: center; ">
+          <div style="float: left; background-color: rgb(215, 215, 215);  background-image: url('https://svgsilh.com/svg/1297477-9e9e9e.svg'); width: 75px; height: 100%; background-size: 100% 100%;">
+            &nbsp;
+          </div>
+          <span style="float: left; color: rgb(200, 200, 200);" class="material-icons">
+            keyboard_arrow_down
+          </span>
+          <div class="hoverable" style="float: left; ">
+            Для граждан
+          </div>
+          <span style="float: left; color: rgb(200, 200, 200);" class="material-icons">
+            keyboard_arrow_down
+          </span>
+        </div>
+        <div>
+          <span class="material-icons" style="float: left; color: rgb(200, 200, 200);">
+            location_on
+          </span>
+          <div class="hoverable" style="float: left; ">
+            Электросталь г.
+          </div>
+          <img style="margin: 0px 15px; float: left;" width="25px" src="../assets/flag.png" alt="A">
+          <div class="hoverable" style="float: left; ">
+            RUS
+          </div>
+          <span style="float: left; color: rgb(200, 200, 200);" class="material-icons">
+            keyboard_arrow_down
+          </span>
+        </div>
+      </div>
+    </div>
+    
+    <div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between; width: 100%; height: 75px; background-color: rgb(255, );">
+      <div style="background-image: url('https://gu-st.ru/st/img/logo_nobeta.0a1f5dfe.svg'); height: 100%; width: 175px; background-size: 100% 100%;">
+      </div>
+      <div style="width: 250px; display: flex; flex-direction: row;  justify-content: space-between;">
+        <p>Услуги</p>
+        <p>Оплата</p>
+        <p>Поддержка</p>
+        <span class="material-icons">
+          search
+        </span>
+      </div>
+      <p>Личный кабинет</p>
+    </div>
+    
+    <div style="display: flex; justify-content: center; align-items: center; background-color: rgb(175, 255, 255); height: 75px; width: 100%;">
+      <div style="display: flex; align-items: center; background-color: rgb(255, 255, 255); height: 75px; width: 25%;">
+        <p style="color: rgb(255, 150, 0);" class="material-icons-outlined">
+          info
+        </p>
+        <p style="margin-left: 15px;">Попробуйте бета-версию новых Госуслуг</p>
+      </div>
+    </div>
+    
+    <div style="width: 100%; height: 475px; background-color: rgb(100, 0, 245);">
+      <div style="width: 85%; margin: auto; display: flex; justify-content: space-around;">
+        <div style="width: 65%;">
+          <input type="text" class="form-control">
+          <p style="color: rgb(255, 255, 255); font-size: 18px; font-weight: bolder;">
+            Рекомендуем для жителей Московской области
+          </p>
+          <div style="display: flex; justify-content: space-around;">
+            <div>
+              <p style="color: rgb(255, 255, 255);">
+                Проверка штрафов
+              </p>
+              <p style="color: rgb(255, 255, 255);">
+                Справка об отсутствии судимости
+              </p>
+            </div>
+            <div>
+              <p style="color: rgb(255, 255, 255);">
+                Получение загранпаспорта
+              </p>
+              <p style="color: rgb(255, 255, 255);">
+                Запись к врачу
+              </p>
+            </div>
+            <div>
+              <p style="color: rgb(255, 255, 255);">
+                Родители и дети
+              </p>
+              <p style="color: rgb(255, 255, 255);">
+                Восстановление документов
+              </p>
+            </div>
+          
+          </div>
+          <hr />
+          <div style="width: 100%; display: flex; justify-content: space-around; align-items: center">
+            <img src="https://gu-st.ru/content/Banner/ic_gasification_main.svg" alt="">
+            <div style="color: rgb(255, 255, 255);">
+              <p>
+                Подайте заявку на подключение к газу
+              </p>
+              <p>
+                Если участок находится в газифицированном населённом пункте
+              </p>
+            </div>
+            <span style="align-self: flex-start; color: rgb(255, 255, 255);" class="material-icons-outlined">
+              close
+            </span>
+          </div>
+          <div style="display: flex; align-items: center">
+            <span  style="color: rgb(255, 255, 255);" class="material-icons-outlined">
+              chevron_left
+            </span>
+            <span style="margin: 0px 5px; color: rgb(255, 255, 255); font-size: 12px;" class="material-icons">
+              circle
+            </span>
+            <span style="margin: 0px 5px; color: rgb(255, 255, 255); font-size: 12px;" class="material-icons-outlined">
+              circle
+            </span>
+            <span style="margin: 0px 5px; color: rgb(255, 255, 255); font-size: 12px;" class="material-icons-outlined">
+              circle
+            </span>
+            <span style="margin: 0px 5px; color: rgb(255, 255, 255); font-size: 12px;" class="material-icons-outlined">
+              circle
+            </span>
+            <span style="margin: 0px 5px; color: rgb(255, 255, 255); font-size: 12px;" class="material-icons-outlined">
+              circle
+            </span>
+            <span style="margin: 0px 5px; color: rgb(255, 255, 255); font-size: 12px;" class="material-icons-outlined">
+              circle
+            </span>
+            <span style="margin: 0px 5px; color: rgb(255, 255, 255); font-size: 12px;" class="material-icons-outlined">
+              circle
+            </span>
+            <span style="color: rgb(255, 255, 255);" class="material-icons-outlined">
+              chevron_right
+            </span>
+          </div>
+        </div>
+        <div style="width: 25%;">
+          <p style="color: rgb(255, 255, 255); font-size: 18px; font-weight: bolder;">Вход в госуслуги</p>
+          <hr/>
+          <p style="color: rgb(255, 255, 255); ">Войдите или зарегистрируйтесь, чтобы получить полный доступ к электронным госуслугам</p>
+          <button style="display: block; width: 250px; padding: 10px; margin: 10px auto" class="btn btn-light">Войти</button>
+          <button style="display: block; margin: auto; width: 250px; padding: 10px;" class="btn btn-primary">Зарегистрироваться</button>
+          <a style="text-align: center; display: block; margin: auto; color: rgb(255, 255, 255); " href="">Как зарегистрироваться</a>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+<style scoped>
+  .hoverable {
+    color: rgb(255, 255, 255);
+  }
+  .hoverable:hover {
+    color: rgb(255, 255, 255);
+  }
+</style>
